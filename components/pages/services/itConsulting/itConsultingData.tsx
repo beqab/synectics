3eda6e1c2139951a700361265d1652934dddc365
beka <badagadzeb@gmail.com>
2022-09-16 18:59:30 +0400

    ff

---
+++ components/pages/services/itConsulting/itConsultingData.tsx
@@ -0,0 +1,134 @@
+export const ItConsultingData = [
+  {
+    title: "Technology Consulting",
+    href: "TechnologyConsulting",
+    description: () => {
+      return (
+        <>
+          We at Synectics, have a very well-coordinated and dynamic delivery
+          team who can perfectly harmonize the advanced, scalable solutions to
+          make your business stand apart from the crowd. Starting from building
+          the digital establishment for your business to incorporating the best
+          engineering productivity tools and practices into your software
+          platform - we do it all with the utmost efficiency.
+        </>
+      );
+    },
+    weCanHelpWithList: [
+      "Analysis and Consulting",
+      "UI/UX",
+      "Custom Web Solution Development",
+      "MVP Development",
+      "SaaS Application Development",
+      "Cloud Native Applications Development",
+    ],
+    technologyList: [
+      {
+        technologyName: "php",
+        technologyImg: "php.svg",
+      },
+      {
+        technologyName: "php",
+        technologyImg: "php.svg",
+      },
+      {
+        technologyName: "php",
+        technologyImg: "php.svg",
+      },
+      {
+        technologyName: "php",
+        technologyImg: "php.svg",
+      },
+      {
+        technologyName: "php",
+        technologyImg: "php.svg",
+      },
+      {
+        technologyName: "php",
+        technologyImg: "php.svg",
+      },
+      {
+        technologyName: "php",
+        technologyImg: "php.svg",
+      },
+      {
+        technologyName: "php",
+        technologyImg: "php.svg",
+      },
+      {
+        technologyName: "php",
+        technologyImg: "php.svg",
+      },
+      {
+        technologyName: "php",
+        technologyImg: "php.svg",
+      },
+    ],
+  },
+  {
+    title: "Process Optimization Consulting",
+    href: "ProcessOptimizationConsulting",
+    description: () => {
+      return (
+        <>
+          We at Synectics, have a very well-coordinated and dynamic delivery
+          team who can perfectly harmonize the advanced, scalable solutions to
+          make your business stand apart from the crowd. Starting from building
+          the digital establishment for your business to incorporating the best
+          engineering productivity tools and practices into your software
+          platform - we do it all with the utmost efficiency.
+        </>
+      );
+    },
+    weCanHelpWithList: [
+      "Analysis and Consulting",
+      "UI/UX",
+      "Custom Web Solution Development",
+      "MVP Development",
+      "SaaS Application Development",
+      "Cloud Native Applications Development",
+    ],
+    technologyList: [
+      {
+        technologyName: "php",
+        technologyImg: "php.svg",
+      },
+      {
+        technologyName: "php",
+        technologyImg: "php.svg",
+      },
+      {
+        technologyName: "php",
+        technologyImg: "php.svg",
+      },
+      {
+        technologyName: "php",
+        technologyImg: "php.svg",
+      },
+      {
+        technologyName: "php",
+        technologyImg: "php.svg",
+      },
+      {
+        technologyName: "php",
+        technologyImg: "php.svg",
+      },
+      {
+        technologyName: "php",
+        technologyImg: "php.svg",
+      },
+      {
+        technologyName: "php",
+        technologyImg: "php.svg",
+      },
+      {
+        technologyName: "php",
+        technologyImg: "php.svg",
+      },
+      {
+        technologyName: "php",
+        technologyImg: "php.svg",
+      },
+    ],
+  },
+];
