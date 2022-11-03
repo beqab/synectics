80e4134d34fb95b281e679e4e946616e950d1c04
beka <badagadzeb@gmail.com>
2022-11-03 17:14:07 +0400

    ff

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,8 +9,9 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>site Title</title>
+        <title>Synectics</title>
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+        <link rel="icon" href="/imgs/logggo.svg" />
 
         {/* <link rel="stylesheet" href="owl-carousel/owl.theme.css"></link> */}
         {/* <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"> */}
